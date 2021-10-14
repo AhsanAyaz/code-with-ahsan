771d0dc22226bb4efad216283f2628fa4b7927ba
Nofal Saleem <nofalsaleemkhan@gmail.com>
2021-10-14 14:48:25 +0500

    Neww mockup updated

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburgur= document.getElementById('hamburgur');
+const topnavul= document.getElementById('topnavul');
+hamburgur.addEventListener('click', () => {
+topnavul.classList.toggle('show');
+
+} );
--- src/utils/index.js
+++
@@ -1,3 +0,0 @@
-export { default as SEO } from "./seo"
-export { default as ThemeContext } from "./theme"
-export { default as Utils } from "./helpers"
