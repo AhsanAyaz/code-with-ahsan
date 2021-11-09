c41203f82ee5727f879584662772fa162009434a
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-11-10 00:23:22 +0100

    feat(login): make the register and login UI

--- pages/register.js
+++ pages/register.js
@@ -1,41 +1,16 @@
 import siteMetadata from '@/data/siteMetadata'
 import { PageSEO } from '@/components/SEO'
-import AmazonGearItems from '../components/AmazingGeatItems'
-import dynamic from 'next/dynamic'
-import { useEffect } from 'react'
-import { YclLogin } from 'your-courses-library-react'
-export default function RegisterPage() {
+import { RegisterPage } from 'your-courses-library-react'
+export default function Register() {
   const registerSubmit = ({ detail }) => {
     const { email, password } = detail
     console.log(email, password)
   }
 
-  const listenForSubmit = () => {
-    document.querySelector('ycl-register').addEventListener('registerSubmit', () => {
-      alert('WHAT')
-    })
-  }
-
-  useEffect(() => {
-    // if (process.browser) {
-    //   import('your-courses-library/dist/custom-elements').then((m) => {
-    //     console.log(m)
-    //     m.defineCustomElements()
-    //   })
-    // }
-    // listenForSubmit()
-  })
-
   return (
     <>
       <PageSEO title={`Register - ${siteMetadata.author}`} description={siteMetadata.description} />
-      {/* <div
-        className="container"
-        dangerouslySetInnerHTML={{
-          __html: '<ycl-register />',
-        }}
-      ></div> */}
-      <YclLogin onFormSubmitted={registerSubmit} />
+      <RegisterPage onFormSubmitted={registerSubmit} />
     </>
   )
 }
