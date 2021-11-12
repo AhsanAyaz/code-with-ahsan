ffbed2c75a5de97ab00e45ac42b42c4c71f51c1f
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-11-12 15:35:56 +0100

    chore(auth): remove registration and login for now

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import { PageSEO } from '@/components/SEO'
-import { LoginPage } from 'your-courses-library-react'
+// import { LoginPage } from 'your-courses-library-react'
 export default function Login() {
   const loginSubmit = ({ detail }) => {
     const { email, password } = detail
@@ -10,7 +10,7 @@ export default function Login() {
   return (
     <>
       <PageSEO title={`Login - ${siteMetadata.author}`} description={siteMetadata.description} />
-      <LoginPage onFormSubmitted={loginSubmit} />
+      {/* <LoginPage onFormSubmitted={loginSubmit} /> */}
     </>
   )
 }
--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import { PageSEO } from '@/components/SEO'
-import { RegisterPage } from 'your-courses-library-react'
+// import { RegisterPage } from 'your-courses-library-react'
 export default function Register() {
   const registerSubmit = ({ detail }) => {
     const { email, password } = detail
@@ -10,7 +10,7 @@ export default function Register() {
   return (
     <>
       <PageSEO title={`Register - ${siteMetadata.author}`} description={siteMetadata.description} />
-      <RegisterPage onFormSubmitted={registerSubmit} />
+      {/* <RegisterPage onFormSubmitted={registerSubmit} /> */}
     </>
   )
 }
