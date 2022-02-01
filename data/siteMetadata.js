4a1977dcda499aa5dda23373d6b2d9e6d0a492e3
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2022-02-01 01:06:45 +0100

    fix(links): fix gde link

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -16,7 +16,7 @@ const siteMetadata = {
   facebook: 'https://facebook.com/CodeWithAhsan',
   youtube: 'https://youtube.com/c/CodeWithAhsan',
   linkedin: 'https://www.linkedin.com/in/ahsanayaz',
-  gde: 'https://codewithahsan.dev/gde',
+  gde: 'https://ahsanayaz.com/gde',
   ngBook: 'https://ahsanayaz.com/ng-book',
   socials: 'https://bio.link/muhd_ahsanayaz',
   locale: 'en-US',
