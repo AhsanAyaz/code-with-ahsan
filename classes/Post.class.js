f1bf709b3af4492a1ec8eb68305c8f45ea95ab11
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2022-04-13 02:50:58 +0200

    fix(post): fix post video url

--- classes/Post.class.js
+++ classes/Post.class.js
@@ -12,9 +12,27 @@ class Post {
   }
 
   getEmbedUrl() {
-    let vidUrl = this.videoUrl.split('=')[1]
-    vidUrl = vidUrl.split('&')[0]
-    return `https://www.youtube.com/embed/${vidUrl}`
+    let vidUrl
+    try {
+      if (this.videoUrl.includes('youtube')) {
+        vidUrl = this.videoUrl.split('watch?v=')[1]
+        return `https://www.youtube.com/embed/${vidUrl}`
+      } else if (this.videoUrl.includes('vimeo')) {
+        vidUrl = this.videoUrl.split('vimeo.com/')[1]
+      } else if (this.videoUrl.includes('youtu.be')) {
+        vidUrl = this.videoUrl.split('youtu.be/')[1]
+        return `https://www.youtube.com/embed/${vidUrl}`
+      } else if (this.videoUrl.includes('dailymotion')) {
+        vidUrl = this.videoUrl.split('video/')[1]
+      } else if (this.videoUrl.includes('facebook')) {
+        vidUrl = this.videoUrl.split('facebook.com/')[1]
+      } else {
+        vidUrl = this.videoUrl
+      }
+      return this.videoUrl
+    } catch (e) {
+      console.log(e)
+    }
   }
 }
 
