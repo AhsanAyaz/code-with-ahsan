94eb2e0a3205ca1e5e859b0efc2c522d1a656b5d
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2022-12-13 22:30:08 +0100

    feat(script): csv to timeline output

---
+++ scripts/custom/csv-to-timeline.js
@@ -0,0 +1,41 @@
+// Import the package
+const csv = require('csv')
+const fs = require('fs')
+const path = require('path')
+
+const content = fs.readFileSync(path.resolve(__dirname, '../../temp/source.csv'))
+
+// Run the pipeline
+csv
+  .parse(content, {
+    delimiter: ',',
+    relax_quotes: true,
+    columns: true,
+  })
+  // Transform each value into uppercase
+  .pipe(
+    csv.transform((record) => {
+      let recordInTime = record['Record In']
+      recordInTime = recordInTime
+        .split(':')
+        .map((val, i, arr) => {
+          if (i === 0) {
+            return (Number(val) - 1).toString().padStart(2)
+          } else if (i === arr.length - 1) {
+            return ''
+          }
+          return val
+        })
+        .filter((val) => !!val)
+        .join(':')
+      return [`${recordInTime} - ${record['Notes']}`]
+    })
+  )
+  // Convert objects into a stream
+  .pipe(
+    csv.stringify({
+      quoted: false,
+    })
+  )
+  // Print the CSV stream to stdout
+  .pipe(process.stdout)
