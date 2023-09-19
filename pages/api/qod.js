c6688d6a38c813404fe5c1943e6b7958f578e1dd
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-09-19 13:25:22 +0200

    fix(qod): fix response being sent incorrectly

--- pages/api/qod.js
+++ pages/api/qod.js
@@ -74,9 +74,8 @@ export default async function handler(req, res) {
     res.status(404).end()
     return
   }
-  console.log(req.origin)
+  console.log(req)
 
-  res.status(200).json({ success: true })
   switch (method) {
     case 'GET':
       try {
