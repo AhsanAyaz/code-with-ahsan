e7b0359606cf0d02ca664258b924678efeef59bf
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-09-19 14:02:46 +0200

    fix(qod): fix API call with user access token

--- pages/api/qod.js
+++ pages/api/qod.js
@@ -2,14 +2,27 @@ import OpenAI from 'openai'
 
 const {
   OPEN_AI_SECRET,
-  FB_PAGE_ACCESS_TOKEN,
   CODE_WITH_AHSAN_PAGE_ID,
   QOD_EP_API_KEY,
+  FB_USER_ACCESS_TOKEN,
+  FB_USER_ID,
 } = process.env
 const openai = new OpenAI({
   apiKey: OPEN_AI_SECRET,
 })
 import axios from 'axios'
+
+const getFBAccessToken = async () => {
+  const resp = await axios.get(
+    `https://graph.facebook.com/${FB_USER_ID}/accounts?access_token=${FB_USER_ACCESS_TOKEN}`
+  )
+  const { data } = resp.data
+  const { access_token } = data[0]
+  console.log({
+    resp: access_token,
+  })
+  return access_token
+}
 
 const sendMessageToDiscord = async (content) => {
   const webhookURL = process.env.DISCORD_WEBHOOK
@@ -22,16 +35,23 @@ const sendMessageToDiscord = async (content) => {
     return res
   } catch (e) {
     console.error('error posting message to discord: ', e)
+    return null
   }
 }
 
-const createPostForFacebook = async (content) => {
+const createPostForFacebook = async (content, accessToken) => {
   console.log({ content })
-  return await axios.post(
-    `https://graph.facebook.com/${CODE_WITH_AHSAN_PAGE_ID}/feed?message=${encodeURIComponent(
-      content
-    )}&access_token=${FB_PAGE_ACCESS_TOKEN}`
-  )
+  try {
+    const resp = await axios.post(
+      `https://graph.facebook.com/${CODE_WITH_AHSAN_PAGE_ID}/feed?message=${encodeURIComponent(
+        content
+      )}&access_token=${accessToken}`
+    )
+    return resp
+  } catch (e) {
+    console.log('Failed to post to facebook: ', e.response)
+    return null
+  }
 }
 
 const getContentFromChatGPT = async () => {
@@ -41,7 +61,7 @@ const getContentFromChatGPT = async () => {
       content: `As a tech-content creator, and a software architect and fullstack developer,
         write a short facebook post that the followers can engage with. Ask a question or something that everyone can respond to.
         Keep it short. 350 characters max. Keep it sober and not too overwhelming with emojis. Start with "Hey everyone 👋🏽, it's time for the question of the day 🚀...\n" Use good hashtags for SEO.
-        Do not mention what you built or didn't. Just ask about the audience's activity, likings, things they find difficult etc. Make it engaging.
+        Do not mention what you built or didn't. Just ask about the audience's activity, likings, things they find difficult etc. You can ask them to show off their projects. Make sure to ask a new question every day. Make it engaging.
         Include the following hashtags at the end (you can add your own too). #codewithahsan #community #questionOfTheDay.
         `,
     },
@@ -56,15 +76,22 @@ const getContentFromChatGPT = async () => {
 }
 
 const sendQuestionOfTheDay = async () => {
+  const fbPageAccessToken = await getFBAccessToken()
   const content = await getContentFromChatGPT()
-  const resFacebook = await createPostForFacebook(content)
+  if (fbPageAccessToken === null) {
+    return
+  }
+  const resFacebook = await createPostForFacebook(content, fbPageAccessToken)
+  if (resFacebook === null) {
+    return
+  }
   const resDiscord = await sendMessageToDiscord(content)
 
   const responses = {
     resFacebook,
     resDiscord,
   }
-  console.log(responses)
+  console.log({ responses })
   return responses
 }
 
