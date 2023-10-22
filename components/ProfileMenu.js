0d7a839bd224623456fb368e74298f4a285b2a17
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-10-22 11:34:52 +0200

    feat(profile-menu): add loading spinner

--- components/ProfileMenu.js
+++ components/ProfileMenu.js
@@ -8,7 +8,7 @@ import { AuthContext } from 'contexts/AuthContext'
 const ProfileMenu = () => {
   const { setShowLoginPopup } = useContext(AuthContext)
   const auth = getAuth(getApp())
-  const [currentUser, setCurrentUser] = useState(null)
+  const [currentUser, setCurrentUser] = useState('loading')
   useEffect(() => {
     const unsub = auth.onAuthStateChanged((user) => {
       setCurrentUser(user)
@@ -28,6 +28,26 @@ const ProfileMenu = () => {
     } else {
       menu.classList.replace('profile-dd--shown', 'profile-dd--hidden')
     }
+  }
+  if (currentUser === 'loading') {
+    return (
+      <div className="overflow-hidden mx-4 relative flex items-center justify-center animate-spin w-10 h-10 bg-gray-100 border border-gray-500 dark:border-transparent rounded-full dark:bg-gray-600">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+          />
+        </svg>
+      </div>
+    )
   }
   if (!currentUser) {
     return (
