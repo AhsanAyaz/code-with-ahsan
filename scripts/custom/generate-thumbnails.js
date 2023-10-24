ae285b10775e6a6175f1be4fc97f3cd369d8b87e
Zain Ahmed <zain.ahmed199524@gmail.com>
2023-10-25 00:43:53 +0800

    update meta placeholder image

--- scripts/custom/generate-thumbnails.js
+++ scripts/custom/generate-thumbnails.js
@@ -35,15 +35,15 @@ async function generateThumbnailFromFrontMatter(post) {
     image
       .print(
         font,
+        105,
         300,
-        450,
         title
           .replace(
             /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
             ''
           )
           .trim(),
-        2000
+        1600
       )
       .write(output)
   })
