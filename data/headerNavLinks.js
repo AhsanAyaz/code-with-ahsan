8ff70c0fb3e4a6b284a46dea96aeed97ec501328
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2024-03-14 17:48:55 +0100

    chore(nav): update header links
    hid tags, gear, and community for now

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,13 +2,14 @@ export const LINKS = {
   ANGULAR_COOKBOOK: 'https://ng-cookbook.com',
 }
 const headerNavLinks = [
+  { href: 'https://codewithahsan.beehiiv.com/', title: 'Newsletter' },
   { href: '/blog', title: 'Blog' },
   { href: '/courses', title: 'Courses' },
   { href: 'https://discord.gg/KSPpuxD8SG', title: 'Discord' },
-  { href: '/tags', title: 'Tags' },
+  // { href: '/tags', title: 'Tags' },
   { href: '/ng-book?source=nav', title: 'Angular Cookbook' },
-  { href: '/gear', title: 'Gear' },
-  { href: '/community', title: 'Community' },
+  // { href: '/gear', title: 'Gear' },
+  // { href: '/community', title: 'Community' },
   { href: '/about', title: 'About' },
 ]
 
