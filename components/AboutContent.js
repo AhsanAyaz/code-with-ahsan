62dabb937340f524e542bdd80a12a8dc43f055bf
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2024-07-28 14:23:49 +0200

    fix(about): fix typo

--- components/AboutContent.js
+++ components/AboutContent.js
@@ -7,7 +7,7 @@ const AboutContent = () => (
     Muhammad Ahsan Ayaz is a Software Architect, Author of the{' '}
     <PrimaryLink href={siteMetadata.ngBook}>Angular Cookbook</PrimaryLink>, and a{' '}
     <PrimaryLink href={siteMetadata.gde}>Google Developers Expert in Angular</PrimaryLink>. He loves
-    helping the startup ecosystem and product owners bringing their ideas to life. And he loves to
+    helping the startup ecosystem and product owners bring their ideas to life. And he loves to
     teach programming using JavaScript, Angular and Web Technologies on{' '}
     <PrimaryLink href={siteMetadata.twitch}>Twitch</PrimaryLink> and{' '}
     <PrimaryLink href={siteMetadata.youtube}>YouTube</PrimaryLink> via Live Streams, video
