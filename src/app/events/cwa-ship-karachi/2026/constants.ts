4c7910a08c9f187bafda2b292a6abd3c7f9bf386
REDFC <saadbandukada@gmail.com>
2026-09-03 22:10:16 +0500

    chore: sponsors position changed

--- src/app/events/cwa-ship-karachi/2026/constants.ts
+++ src/app/events/cwa-ship-karachi/2026/constants.ts
@@ -262,16 +262,16 @@ const SPONSOR_IMG = "/static/images/events/cwa-ship-karachi-2026/sponsors";
 
 export const CONFIRMED_SPONSORS: ConfirmedSponsor[] = [
   {
-    name: "Command Code",
-    logoUrl: `${SPONSOR_IMG}/commandcode.svg`,
-    websiteUrl: "https://commandcode.ai",
-    tier: "Gold Sponsor",
-  },
-  {
     name: "Gorelo",
     logoUrl: `${SPONSOR_IMG}/gorelo.png`,
     websiteUrl: "",
     tier: "Startup Sponsor",
+  },
+  {
+    name: "Command Code",
+    logoUrl: `${SPONSOR_IMG}/commandcode.svg`,
+    websiteUrl: "https://commandcode.ai",
+    tier: "Gold Sponsor",
   },
   {
     name: "MSA KHI",
